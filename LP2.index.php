14a24f75e6b866df5ec2ec06d2c81afe6d1083bb
Lebronthegoat69 <SNoyes2@student.morainepark.edu>
2025-02-15 18:43:54 -0600

    Add files via upload

---
+++ LP2.index.php
@@ -0,0 +1,14 @@
+<?php
+
+
+function displayArrayValues(array $arr)
+{
+    foreach ($arr as $value) {
+        echo "Banana" ($value) . "<br>";
+    }
+}
+$values = ["Apple", "Banana", "Cherry", "Date", "Elderberry"];
+
+// Display the array values
+"Banana"($values);
+?>
